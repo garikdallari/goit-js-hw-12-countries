f78d86dbc62bde171feae0d7faac647c189e8461
garikdallari <garikdallari@gmail.com>
2021-07-08 13:50:11 +0300

    bbugfix

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import countryListTpl from './templates/country-list.hbs';
 import Notiflix from "notiflix";
 import debounce from 'lodash.debounce'
 
+
 const refs = getRefs();
 const DEBOUNCE_DELAY = 300;
 
@@ -18,35 +19,37 @@ function onCountrySearch(e) {
     e.preventDefault();
     const searchQuery = e.target.value;
     
-    fetchCountries(searchQuery).then(updateCountryList)
+    fetchCountries(searchQuery)
+        .then(updateCountryList)
+        // .catch(Notiflix.Notify.failure('Oops, there is no country with that name'));
+        
 }
 
 
 function updateCountryList(data) {
     const renderCountryList = countryListTpl(data);
-    clearData()
-    
-    renderCountryCard(data);
+
+    clearData();
 
     if (data.length >= 2 && data.length <= 10) {
-        clearData()
         refs.countryListContainer.innerHTML = renderCountryList;
         return;
     };
     
     if (data.length > 10) {
-        clearData()
         Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
         return;
     };
-
+ 
     if (data.status === 404) {
-        clearData()
         Notiflix.Notify.failure('Oops, there is no country with that name');
         return;
-
     };
-}
+
+        renderCountryCard(data);
+     
+};
+
 
 function renderCountryCard(country) {
     const countryMarkup = countryCardTpl(country);
@@ -56,7 +59,9 @@ function renderCountryCard(country) {
 function clearData() {
     refs.countryListContainer.innerHTML = '';
     refs.countryContainer.innerHTML = '';
-}
+};
+
+
 
 
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,5 +2,5 @@ export default function fetchCountries(searchQuery) {
     return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}?fields=name;capital;population;flag;languages`)
         .then(res => res.json())
         .then(data => data)
-        .catch(error => console.log('Something goes wrong'));
-};
+        .catch(error => error)
+}
